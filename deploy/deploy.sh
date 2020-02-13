14edee88e711b1489504633bafb89ee68bae8e91
mark100net <mark@pokervane.com>
2020-02-13 10:28:28 -0800

    build nginx image

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,10 +3,12 @@ set -ev
 echo "$TRAVIS_COMMIT" > ./deploy/deploy_hash
 echo "$MASTER_KEY" > ./config/master.key
 
-# build and push image
+# build and push images
 docker build -f ./docker/app/Dockerfile.prod -t babywearing.azurecr.io/babywearing:prod .
+docker build -f ./docker/web/Dockerfile -t babywearing.azurecr.io/nginx:prod .
 docker login babywearing.azurecr.io --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 docker push babywearing.azurecr.io/babywearing:prod
+docker push babywearing.azurecr.io/nginx:prod
 
 # deploy to vm - we set vars below and not just in Travis web ui because they can't be set in build phase above,
 # otherwise the image would try to be built on the vm and we do not want that
